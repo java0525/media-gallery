a90408d9dbbec96331bd0f2dc3158eac8e5d448f
Javokhir Bekpulatov <75106444+java0525@users.noreply.github.com>
2021-11-29 14:59:49 -0500

    Add files via upload

---
+++ Media-gallery 2/js/main.js
@@ -0,0 +1,69 @@
+ // NAV BAR HAMBURGER
+
+ $(document).ready(function() {
+   $('.btn').click(function() {
+     $('.items').toggleClass("show");
+     $('ul li').toggleClass("hide");
+   });
+ });
+
+ // NAV BAR HAMBURGER
+
+
+
+
+ // gallery
+ var slideIndex = 1;
+ showSlides(slideIndex);
+
+ // Next/previous controls
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+
+ // Thumbnail image controls
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("demo");
+   var captionText = document.getElementById("caption");
+   if (n > slides.length) {
+     slideIndex = 1
+   }
+   if (n < 1) {
+     slideIndex = slides.length
+   }
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
+   captionText.innerHTML = dots[slideIndex - 1].alt;
+ }
+
+
+
+
+
+
+ //////
+
+
+ // Image modal functions - Requires jQuery!!!
+ $(".button").on("click", function() { // when item with class of button is clicked, fire function
+   var modal = $(this).data("modal"); // sets modal var equal to data attribute
+   $(modal).show(); // opens up modal (much code hidden here by jQuery)
+ });
+ $(".modal").on("click", function(e) { // sets up click function
+   var className = e.target.className; // var className set to event target
+   if (className === "modal" || className === "close") { // if conditions are met...
+     $(this).closest(".modal").hide(); // then hide the modal
+   } // closes if-statement
+ }); // closes function
